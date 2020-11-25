8ff7a000865b4b7b2618efb29a0bda6770b5be7d
Michael Saterson <saterson@gmail.com>
2020-11-24 21:20:09 -0500

    create install script inspired by gnu stow

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+find . -name 'dot-*' -execdir bash -c '
+    function symlink_to_home {
+        file=$1
+        echo "Creating symlink" $HOME/.${file:4} "->" $PWD/$1
+        ln -s $1 $HOME/.${file:4}
+    }
+    symlink_to_home "$@"' bash {} \;
