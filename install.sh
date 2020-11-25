6fc6e6625a6af673837430eec6bb183c5c9c8c2e
Michael Saterson <saterson@gmail.com>
2020-11-24 21:33:55 -0500

    tweak install script, add gitignore

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ find . -name 'dot-*' -execdir bash -c '
     function symlink_to_home {
         file=$1
         echo "Creating symlink" $HOME/.${file:4} "->" $PWD/$1
-        ln -s $1 $HOME/.${file:4}
+        ln -s $PWD/$1 $HOME/.${file:4}
     }
     symlink_to_home "$@"' bash {} \;
